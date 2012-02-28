7c6487e2c2e112ebe1b00629940a009d2b600c90
happyness <hissaal-bahr@dyn-86-36-43-111.wv.qatar.cmu.edu>
2012-02-28 16:57:41 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testit::Application.config.secret_token = '343b2b86d067e1d47a9974a063e66ce521b7061a96fdd46fe2a83294b2181775dc752a0991841fdc440989b19ab8c7e62ea91401dca95e5f7d994988c85516da'
